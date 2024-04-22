a533c271f7e4e4ba5ac49c290f5b7fdf06b958cc
DongjinSon9 <dongj1358@naver.com>
2024-04-22 12:50:55 +0900

    4교시 - pageResponseDTO 만들기 2

--- springex/src/main/java/org/zerock/springex/dto/PageResponseDTO.java
+++ springex/src/main/java/org/zerock/springex/dto/PageResponseDTO.java
@@ -1,5 +1,7 @@
 package org.zerock.springex.dto;
 
+import lombok.Builder;
+
 import java.util.List;
 
 public class PageResponseDTO<E> {
@@ -21,6 +23,11 @@ public class PageResponseDTO<E> {
     private List<E> dtoList;
 
 //    생성자 만들기. 파라미터 3개짜리.
+//    페이지 계산방법
+//    예)
+//    기본, 1,2,3,4,5,6,7,8,9,10
+//    페이지2, 2,3,4,5,6,7,8,9,10,11
+//    페이지10, 10,11, 12, 13, 14, 15, 16, 17, 18, 19
     public PageResponseDTO(PageRequestDTO pageRequestDTO, List<E> dtoList, int total) {
 
         this.page = pageRequestDTO.getPage();
@@ -28,6 +35,21 @@ public class PageResponseDTO<E> {
 
         this.total = total;
         this.dtoList = dtoList;
+
+//        end 페이지 먼저 계산하기
+//        예)
+//        1/10 = 0.1 -(올림)-> 1 => 10
+//        예2)
+//        11페이지, 11/10 = 1.1 -> 2 => 20
+        this.end = (int)(Math.ceil(this.page / 10.0))*10;
+
+//        start 페이지 구하기
+//        예)
+//        end = 30 , start = 21
+        this.start = this.end - 9;
+
+//        last 값이 없어서 생성 후 end 멤버에 재사용하기
+        int last = (int)(Math.ceil(total / (double) size));
 
     }
 
